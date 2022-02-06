f68f5c7be2089da3eb45b2e5440a7d5ce60b4025
Narek <hovsepyan090990@gmail.com>
2022-02-06 13:25:05 -0800

    problem with pplaceholder

--- src/Components/placeholder/PlaceHolder.js
+++ src/Components/placeholder/PlaceHolder.js
@@ -4,6 +4,20 @@ import Placeholder1 from "../../images/Placeholder1.png"
 
 export const PlaceHolder=()=> {
   return <div className='placeholderMain'>
+    <div className='placeholder'>
+    <p className='slidetitle'>Placeholder</p>
 <img alt="" src={Placeholder1}/>
+<p>Anatomy of an Artist: Aleksandra Domanović</p>
+</div>
+<div className='placeholder'>
+    <p className='slidetitle'>Placeholder</p>
+<img alt="" src={Placeholder1}/>
+<p>Meet the artists by Art Basel: Bárbara Wagner and Benjamin de Burca</p>
+</div>
+<div className='placeholder'>
+    <p className='slidetitle'>Placeholder</p>
+<img alt="" src={Placeholder1}/>
+<p>Anatomy of an Artist: Aleksandra Domanović</p>
+</div>
   </div>;
 }
