4ec596250cf3d2715442e998331de69aa7774c34
Narek <hovsepyan090990@gmail.com>
2022-02-11 15:31:13 +0400

    some errors

--- src/Components/climate/Climate.js
+++ src/Components/climate/Climate.js
@@ -4,11 +4,7 @@ import "./climate.css"
 
 export const Climate=(props)=> {
     return <div className="climatmain">
-         {
-      props.climeTitle && (
-        <p className='text2' >Environmental activist Satish </p>
-      )
-    }
+      
        {
       props.title && (
         <p className='mostwatched' >{ props.title} </p>
@@ -18,6 +14,11 @@ export const Climate=(props)=> {
    {
       props.parenttitle && (
         <p className='parenttitle' >{ props.parenttitle} </p>
+      )
+    }
+       {
+      props.climeTitle && (
+        <p className='text2' >Environmental activist Satish </p>
       )
     }
    <p className="text1">{props.text}</p>
